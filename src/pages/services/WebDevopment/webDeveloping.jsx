f0ef19b280574f82aa019f4acc5b45b17e973a5a
Vidyadhar chaudhary <105207209+Erroug@users.noreply.github.com>
2025-09-18 12:08:41 +0530

    web developing responsiveness

--- src/pages/services/WebDevopment/webDeveloping.jsx
+++ src/pages/services/WebDevopment/webDeveloping.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const webDeveloping = () => {
+const WebDeveloping = () => {
   return (
     <div className="flex flex-col items-center justify-center px-4">
       {/* Our Cityportals Section */}
@@ -15,7 +15,7 @@ const webDeveloping = () => {
                 <img
                   src="/assets/website-developing/711px-PHP-logo.svg_-oebs5gomsurkxvkt6s607r884j3us42w0f4rwrcem8.png"
                   alt="PHP"
-                  className="max-w-[120px] md:max-w-[150px] h-auto"
+                  className="w-2/3 max-w-[150px] h-auto"
                 />
               </div>
               <p className="text-white leading-6">
@@ -31,14 +31,13 @@ const webDeveloping = () => {
                 <img
                   src="/assets/website-developing/Vertical-Logo-2015-e1569483208477-oebsv5ltltxqa69b5vywd9tsnoh16a2rjl2k5190jk.png"
                   alt="PrestaShop"
-                  className="max-w-[120px] md:max-w-[150px] h-auto"
+                  className="w-2/3 max-w-[150px] h-auto"
                 />
               </div>
-             
               <p className="text-white leading-6">
-              PrestaShop is a freemium, open source e-commerce solution. The software 
-              is published under the Open Software License. It is written in the PHP programming
-               language with support for the MySQL database management system.
+                PrestaShop is a freemium, open source e-commerce solution. The software 
+                is published under the Open Software License. It is written in the PHP programming
+                language with support for the MySQL database management system.
               </p>
             </div>
 
@@ -48,10 +47,9 @@ const webDeveloping = () => {
                 <img
                   src="/assets/website-developing/1200px-.NET_Core_Logo.svg_-oebsjulpekgkn0oi6i1fvmj7fu73lc6znmlbc80hds.png"
                   alt=".NET"
-                  className="max-w-[120px] md:max-w-[150px] h-auto"
+                  className="w-2/3 max-w-[150px] h-auto"
                 />
               </div>
-           
               <p className="text-white leading-6">
                 The DOT NET Framework’s Base Class Library offers user interface,
                 database connectivity, data access, cryptography, web application
@@ -65,13 +63,12 @@ const webDeveloping = () => {
                 <img
                   src="/assets/website-developing/download-6-oebtntom1x6ke0m5ra39f1dwwr61xxw1hhdp2uqqrk.png"
                   alt="Angular"
-                  className="max-w-[120px] md:max-w-[150px] h-auto"
+                  className="w-2/3 max-w-[150px] h-auto"
                 />
               </div>
-             
               <p className="text-white leading-6">
-              Angular is a TypeScript-based open-source web application framework led by the Angular Team at 
-              Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS.
+                Angular is a TypeScript-based open-source web application framework led by the Angular Team at 
+                Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS.
               </p>
             </div>
 
@@ -81,13 +78,12 @@ const webDeveloping = () => {
                 <img
                   src="/assets/website-developing/download-5-oebtly08dulx63cgqgu4figq50fmjqfd86eqixj37k.png"
                   alt="Shopify"
-                  className="max-w-[120px] md:max-w-[150px] h-auto"
+                  className="w-2/3 max-w-[150px] h-auto"
                 />
               </div>
-             
               <p className="text-white leading-6">
-              Shopify Inc. is a Canadian multinational e-commerce company headquartered in Ottawa, Ontario. It is also the name of 
-              its proprietary e-commerce platform for online stores and retail point-of-sale systems.
+                Shopify Inc. is a Canadian multinational e-commerce company headquartered in Ottawa, Ontario. It is also the name of 
+                its proprietary e-commerce platform for online stores and retail point-of-sale systems.
               </p>
             </div>
 
@@ -97,13 +93,12 @@ const webDeveloping = () => {
                 <img
                   src="/assets/website-developing/ezgif-4-717ef4fc7b7f-oebu1feiylsyeuutbnrw03t2ei4ac8w70t9o40kmps.png"
                   alt="Magento"
-                  className="max-w-[120px] md:max-w-[150px] h-auto"
+                  className="w-2/3 max-w-[150px] h-auto"
                 />
               </div>
-             
               <p className="text-white leading-6">
-              Magento is an open-source e-commerce platform written in PHP. The software
-               was originally developed by Varien, Inc, a US private company headquartered
+                Magento is an open-source e-commerce platform written in PHP. The software
+                was originally developed by Varien, Inc, a US private company headquartered
                 in Culver City, California, with assistance from volunteers. Varien published 
                 the first general-availability release of the software on March 31, 2008.
               </p>
@@ -115,13 +110,12 @@ const webDeveloping = () => {
                 <img
                   src="/assets/website-developing/download-4-oebtaqrgxx9wtdm754j67u7zapn5tkyioqjfn84zcw.png"
                   alt="Joomla"
-                  className="max-w-[120px] md:max-w-[150px] h-auto"
+                  className="w-2/3 max-w-[150px] h-auto"
                 />
               </div>
-              
               <p className="text-white leading-6">
-              Joomla is a free and open-source content management system for publishing web content, developed by 
-              Open Source Matters, Inc. It is built on a model–view–controller web application framework that can be used independently of the CMS.
+                Joomla is a free and open-source content management system for publishing web content, developed by 
+                Open Source Matters, Inc. It is built on a model–view–controller web application framework that can be used independently of the CMS.
               </p>
             </div>
 
@@ -131,13 +125,12 @@ const webDeveloping = () => {
                 <img
                   src="/assets/website-developing/download-8-oebtve9t3vjpwxm7jjx2i4ll6huhy5y76yki93ikn4.png"
                   alt="WordPress"
-                  className="max-w-[120px] md:max-w-[150px] h-auto"
+                  className="w-2/3 max-w-[150px] h-auto"
                 />
               </div>
-              
               <p className="text-white leading-6">
-              WordPress is a content management system based on PHP and MySQL that is usually used with the MySQL or MariaDB
-               database servers but can also use the SQLite database engine. Features include a plugin architecture and a template system.
+                WordPress is a content management system based on PHP and MySQL that is usually used with the MySQL or MariaDB
+                database servers but can also use the SQLite database engine. Features include a plugin architecture and a template system.
               </p>
             </div>
 
@@ -148,4 +141,4 @@ const webDeveloping = () => {
   )
 }
 
-export default webDeveloping
+export default WebDeveloping
